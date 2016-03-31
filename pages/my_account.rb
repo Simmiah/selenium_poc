7b2477f4e4adb59047bb0a1a9eea5a193c1663c1
Rosalyn Goh <rosalyn.goh@gmail.com>
2016-03-31 09:35:49 -0700

    myacount first commit, created page and spec

---
+++ pages/my_account.rb
@@ -0,0 +1,14 @@
+# filename: my_account.rb
+
+require_relative 'base_page'
+
+class MyAccount < BasePage
+
+	ORDER_HISTORY = {  }
+	MY_CREDIT = {  }
+	MY_ADDRESSES = {  }
+	MY_INFORMATION = {  }
+	MY_WISHLISTS = {  }
+
+
+end
---
+++ spec/myaccount_spec.rb
@@ -0,0 +1,10 @@
+# filename: myaccount_spec.rb
+
+require_relative 'spec_helper'
+require_relative '../pages/create_account'
+
+describe 'My Account' do
+
+
+
+end
