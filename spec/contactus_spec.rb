ec7e313af5b82f6960114db390c9529defe19cee
Rosalyn Goh <rosalyn.goh@gmail.com>
2016-04-13 21:57:52 -0700

    checking spaces

--- spec/contactus_spec.rb
+++ spec/contactus_spec.rb
@@ -5,28 +5,28 @@ require_relative '../pages/contact_us'
 
 describe 'Contact Us' do
 
-	before(:each) do
-		@contactus = ContactUs.new(@driver)
-	end
+  before(:each) do
+    @contactus = ContactUs.new(@driver)
+  end
 =begin
-	it 'should show contact us header' do
-		expect(@contactus.contact_us_header_displayed?).to be true
-	end
+  it 'should show contact us header' do
+    expect(@contactus.contact_us_header_displayed?).to be true
+  end
 
-	it 'should update the heading dropdown when selected' do
-		@contactus.choose_displayed?
-		@contactus.service_displayed?
-		@contactus.webmaster_displayed?
-	end
+  it 'should update the heading dropdown when selected' do
+    @contactus.choose_displayed?
+    @contactus.service_displayed?
+    @contactus.webmaster_displayed?
+  end
 =end
-	it 'should upload a file' do
-		@contactus.select_service
-		@contactus.enter_email('test@test.com')
-		@contactus.attach_file('/files/file.txt')
-		@contactus.enter_message('test')
-		@contactus.send_message
-		expect(@contactus.success_displayed?)
-	end
+  it 'should upload a file' do
+    @contactus.select_service
+    @contactus.enter_email('test@test.com')
+    @contactus.attach_file('/files/file.txt')
+    @contactus.enter_message('test')
+    @contactus.send_message
+    expect(@contactus.success_displayed?)
+  end
 
 
 
